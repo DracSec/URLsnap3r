23d7c04618b514e37d2c9233864993eaa6743e37
DracSec <dragown.pt@gmail.com>
2026-02-27 20:55:13 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 python -m venv ../URLsnap3r
 ./bin/pip install requests beautifulsoup4 playwright
 ./bin/playwright install
-ln -s $(pwd)/urlsnap3r.py /usr/bin/urlsnap3r
+ln -s $(pwd)/urlsnap3r.py ~/.local/bin/urlsnap3r
